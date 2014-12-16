7f515925da888e6a93b8b7f89059c9d108441a43
Tijana Srejic <tijana_srejic@outlook.com>
2014-12-16 01:37:22 +0100

    base makefile

---
+++ rs232_mod.c
@@ -0,0 +1,16 @@
+#include <linux/module.h> /* Needed by all modules. */
+#include <linux/kernel.h> /* Needed for KERN_INFO. */
+
+int init_module(void) //za ukljucivanje modula
+{
+    printk(KERN_INFO "Hello world 1\n");
+
+	/* A non 0 return means init_module failed - module can't be loaded. */
+
+	return 0;
+}
+
+void cleanup_module(void) //za iskljucivanje modula
+{
+	printk(KERN_INFO "Goodbye world 1\n");
+}
