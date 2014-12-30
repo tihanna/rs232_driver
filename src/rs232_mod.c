4d1c474072810212460d0e18ce038edcacb62772
Nikola Hardi <atlantic777@lugons.org>
2014-12-31 00:29:22 +0100

    Simple read and write functions

--- src/rs232_mod.c
+++ src/rs232_mod.c
@@ -7,8 +7,8 @@
 
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
-//static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-//static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
 MODULE_LICENSE("GPL");
 
@@ -24,21 +24,25 @@ static char *msg_ptr;
 int fctrl = 0;
 
 struct file_operations fops = {
-//read: device_read,
-//write: device_write,
+read: device_read,
+write: device_write,
 open: device_open,
 release: device_release
 };
 
-
 int init_module(void) {
     printk(KERN_INFO "Hello world\n");
 	major = register_chrdev(0, DEVICE_NAME, &fops);
+    printk(KERN_INFO "Major is: %d\n", major);
+
 	if (major < 0) {
 		printk(KERN_ALERT "Registering char device failed with %d\n", major);
 
 		return major;
 	}
+
+    // create device node
+
 	request_mem_region(UART_BASE_ADDRRESS, 0x20, DEVICE_NAME);
 	ioremap(UART_BASE_ADDRRESS, 0x20);
 
@@ -57,6 +61,8 @@ int init_module(void) {
     iowrite8(fctrl | FIFO_ENABLE_BIT, UART_FIFO_CTRL);
 
     iowrite8(65, UART_TX_DATA);
+
+    // create and setup timer
 
 	return 0;
 }
@@ -91,3 +97,37 @@ static int device_release(struct inode *inode, struct file *file) {
 	return 0;
 }
 
+void timer_handler()
+{
+    // check fifo and buffer state
+    // read into the buffer
+    // anything to send?
+    // send it via uart
+}
+
+static ssize_t device_read(struct file *f, char *c, size_t s, loff_t *off){
+    // check your buffer
+    char buff[10];
+    int i;
+
+    for(i = 0; i < 5; i++) {
+        buff[i] = ioread8(UART_RX_DATA);
+    }
+
+    buff[6] = 0;
+
+
+    printk(KERN_INFO "reading... %s\n", buff);
+
+    return 0;
+}
+
+static ssize_t device_write(struct file *f, const char *c, size_t s, loff_t *off){
+    int i;
+    for(i = 0; i < s; i++) {
+        printk(KERN_INFO "writing...\n");
+        iowrite8(c[i], UART_TX_DATA);
+    }
+
+    return s;
+}
