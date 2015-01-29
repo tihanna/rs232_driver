eccb1f8143352e4bb9e6e13bce22e1d354108170
Nikola Hardi <atlantic777@lugons.org>
2015-01-29 12:25:59 +0100

    Before demo

--- src/rs232_mod.c
+++ src/rs232_mod.c
@@ -25,7 +25,7 @@ static irqreturn_t irqh(int, void*, struct pt_regs*);
 
 MODULE_LICENSE("GPL");
 
-#define DEVICE_NAME "rs232_mod"
+#define DEVICE_NAME "rs232"
 #define SUCCESS 0
 #define BUF_LEN 80
 
@@ -34,6 +34,7 @@ static int Device_Open  =   0;
 int irq_resp;
 
 int fctrl = 0;
+int baudrate = BAUD_DIVISOR; // default baudrate
 
 struct file_operations fops = {
 read: device_read,
@@ -44,6 +45,9 @@ release: device_release
 
 struct kfifo rx_fifo;
 wait_queue_head_t inq;
+
+// module_param(baudrate, int, 0644);
+// MODULE_PARM_DESC(baudrate, "Device baudrate")
 
 static struct class *rs232_device_class = 0;
 
@@ -70,10 +74,6 @@ int init_module(void) {
                                DEVICE_NAME,
                                (void*)(irqh)
                            );
-
-
-    fctrl = ioread8(UART_FIFO_CTRL);
-    iowrite8(fctrl | FIFO_ENABLE_BIT, UART_FIFO_CTRL);
 
 	return 0;
 }
@@ -140,12 +140,11 @@ irqreturn_t irqh(int irq, void *dev_id, struct pt_regs *regs)
 {
     unsigned char input_char;
     int res;
-    printk(KERN_INFO "Interrupt happened\n");
     input_char = ioread8(UART_RX_DATA);
 
     wake_up_interruptible( &inq );
-
     res = kfifo_in(&rx_fifo, &input_char, 1);
+
     return IRQ_HANDLED;
 }
 
@@ -165,14 +164,20 @@ void remap_io()
 
 void init_controller()
 {
+    int baudval;
+
     iowrite8(fctrl & LOOP_BIT, UART_MODEM_CTRL);
 
     iowrite8(DIVISOR_ACCESS_BIT, UART_LINE_CTRL);
 
+    //baudval = ((UART_CLOCK/baudrate+8)/16) & 0xFFFF;
     iowrite8(BAUD_DIVISOR, UART_DLATCH_LO);
     iowrite8(BAUD_DIVISOR>>8, UART_DLATCH_HI);
 
     iowrite8(WORD_LENGTH_8, UART_LINE_CTRL);
 
     iowrite8(0x01, UART_INTR_EN);
+
+    fctrl = ioread8(UART_FIFO_CTRL);
+    iowrite8(fctrl | FIFO_ENABLE_BIT, UART_FIFO_CTRL);
 }
